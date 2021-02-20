8fbe23d7ed2a52d778a47eb1f3782bb437de855d
Tian <tian.qiu142@gmail.com>
2021-02-19 21:01:30 -0800

    added conditional rendering of posts based on the state status of idle, loading, succeeded, and failed. And added PostExcrept component for mapping each posts

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,10 +1,6 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { Link } from 'react-router-dom'
-
-import { PostAuthor } from './PostAuthor'
-import { TimeAgo } from './TimeAgo'
-import { ReactionButtons } from './ReactionButtons'
+import { PostExcrept } from './PostExcrept'
 
 import { selectAllPosts, fetchPosts } from './postsSlice'
 
@@ -12,7 +8,7 @@ export const PostsList = () => {
   const dispatch = useDispatch()
 
   const posts = useSelector(selectAllPosts)
-
+  const error = useSelector((state) => state.posts.error)
   const postStatus = useSelector((state) => state.posts.status)
 
   useEffect(() => {
@@ -21,33 +17,24 @@ export const PostsList = () => {
     }
   }, [postStatus, dispatch])
 
+  let content
+
+  if (postStatus === 'loading') {
+    content = <div className="loader">Loading...</div>
+  } else if (postStatus === 'succeeded') {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date))
+
+    content = orderedPosts.map((post) => <PostExcrept post={post} />)
+  }
+
   // Sort posts in reverse chronological order by datetime string
-  const orderedPosts = posts
-    .slice()
-    .sort((a, b) => b.date.localeCompare(a.date))
-
-  const renderedPosts = orderedPosts.map((post) => {
-    return (
-      <article className="post-excerpt" key={post.id}>
-        <h3>{post.title}</h3>
-        <div>
-          <PostAuthor userId={post.user} />
-          <TimeAgo timestamp={post.date} />
-        </div>
-        <p className="post-content">{post.content.substring(0, 100)}</p>
-
-        <ReactionButtons post={post} />
-        <Link to={`/posts/${post.id}`} className="button muted-button">
-          View Post
-        </Link>
-      </article>
-    )
-  })
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   )
 }
